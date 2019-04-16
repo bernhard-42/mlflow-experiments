4976574fbf9ff486661fea565ebe6629f1a557ec
Bernhard Walter (Databricks) <bernhard.walter@databricks.com>
2019-04-16 18:58:22 +0200

    changed to cloud paths

--- sklearn/train.py
+++ sklearn/train.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     np.random.seed(40)
 
     # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
-    wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
+    wine_path = "/dbfs/FileStore/bernhard/wine-quality.csv")
     data = pd.read_csv(wine_path)
 
     # Split the data into training and test sets. (0.75, 0.25) split.
